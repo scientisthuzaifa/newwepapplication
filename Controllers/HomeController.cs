429a7bb1394855da477665381ce2c407f89a08e1
scientisthuzaifa <pc21721.huzaifakhalid@gmail.com>
2023-12-05 07:54:54 -0800

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+using MyApp.Db.DbOperations;
+using MyApp.Models;
+
+namespace WebApplication14.Controllers
+{
+    public class HomeController : Controller
+    {
+        EmployeeRepository repository = new EmployeeRepository();
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(EmployeeModel model)
+        {
+
+           
+            {
+                int id = repository.AddEmployee(model);
+                if (id == 0)
+                {
+                    ModelState.Clear();
+                    ViewBag.Issuccess = "DataADDED";
+                }
+            }
+            return View();
+        }
+    }
+}
